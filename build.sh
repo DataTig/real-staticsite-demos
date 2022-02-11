8a8386cc72cf569e59bd750991213df30383fb12
James <james.baster@opendataservices.coop>
2022-02-11 14:12:48 +0000

    upforgrabs: Add

--- build.sh
+++ build.sh
@@ -48,3 +48,19 @@ else
 fi
 
 python -m datatig.cli build repos/orgid --staticsiteoutput out/orgid --staticsiteurl /orgid
+
+echo "========================================================================  UPFORGRABS"
+
+if [ -d repos/upforgrabs ]; then
+  cd repos/upforgrabs
+  git pull
+  cd ../..
+else
+  cd repos
+  git clone --branch gh-pages https://github.com/up-for-grabs/up-for-grabs.net.git upforgrabs
+  cd ..
+fi
+
+cp -r configs/upforgrabs/* repos/upforgrabs/
+
+python -m datatig.cli build repos/upforgrabs --staticsiteoutput out/upforgrabs --staticsiteurl /upforgrabs
